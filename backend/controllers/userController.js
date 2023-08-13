16ecca5f5b40512d90ef6f84ba9f6ff7ec3ba2b1
Franklin <71532004+fran1014@users.noreply.github.com>
2023-08-13 11:26:47 +0200

    creating loging functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,8 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/userModel');
+const { error } = require('console');
 
 //@desc Register a new user
 //@route /api/users
@@ -13,7 +17,36 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('Please include all fields');
   }
 
-  res.send('Register Route');
+  //Find if user already exist
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new error('Invalid user data');
+  }
 });
 
 //@desc Login a new user
@@ -21,7 +54,14 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access PUBLIC
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login Route');
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  //Check user and passwords match
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+  }
 });
 
 module.exports = { registerUser, loginUser };
--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,3 @@
-const status = require('statuses');
-
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
   res.status(statusCode);
