14383bb689399a12ced48a89d8d71a8fb7b8735b
Franklin <71532004+fran1014@users.noreply.github.com>
2023-08-14 20:45:24 +0200

    created login and register functionality

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+
+//Register new user
+export const login = createAsyncThunk(
+  '/auth/login',
+  async (user, thunkAPI) => {}
+);
+
+//Login user
+export const register = createAsyncThunk(
+  '/auth/register',
+  async (user, thunkAPI) => {}
+);
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export default authSlice.reducer;
