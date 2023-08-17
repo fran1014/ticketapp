66e7d26bea30e183fa447f2e46a0c41c48cd618c
Franklin <71532004+fran1014@users.noreply.github.com>
2023-08-18 01:51:34 +0200

    doing close ticket functionality

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,10 +1,10 @@
-import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const BackButton = ({ url }) => {
   return (
     <Link to={url} classname="btn btn-reverse btn-back">
-      <FaArrowLeft />
+      <FaArrowAltCircleLeft />
       Back
     </Link>
   );
